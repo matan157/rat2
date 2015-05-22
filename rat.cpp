2d462e50c024732722f2b114bbc00403d5e51f1b
Matan <matan.uchen@gmail.com>
2015-05-21 20:00:03 -0400

    insertion, extraction, constructors work!

--- rat.cpp
+++ rat.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<string>
 #include<cmath>
-#include<cstddef>
+#include<sstream>
 #include"normalize.h"
 #include"rat.h"
 using namespace std;
@@ -10,20 +10,28 @@ using namespace std;
 Rat::Rat() : numerator(0), denominator(1), NaN(false) {}
 Rat::Rat(int x) : numerator(x), denominator(1), NaN(false) {}
 Rat::Rat(int n, int d) {
-    normalize(n,d);
+    if(d == 0) {
+	NaN = true;
+	numerator = 1;
+	denominator = 1;
+	return;
+    }
     if(d < 0) {
 	n = -n;
 	d = -d;
     }
+    normalize(n,d);
     numerator = n;
     denominator = d;
-    d == 0? NaN = true : NaN = false;
+    NaN = false;
 }
 
 Rat::Rat(double x) {
-    int n, d, tmp = 10;
-
-    n = (int)(pow(10,tmp)) * x;
+    int n, d, tmp = 8;
+    double m;
+    
+    m = (pow(10,tmp)) * x;
+    n = (int)m;
     d = (int)(pow(10,tmp));
     normalize(n,d);
     numerator = n;
@@ -33,19 +41,39 @@ Rat::Rat(double x) {
 
 Rat::Rat(string str) {
     bool sep = false;
+    bool paren = false;
+    istringstream iss;
+
+    int n, d;
     Rat r;
     for(int i = 0; i < str.length(); i++) {
 	if(str[i] == '.')
 	    sep = true;
-	if((str[i] < '0' || str[i] > '9') && str[i] != '.') {
+	if((str[i] < '0' || str[i] > '9') && (str[i] != '.'
+	&& str[i] != '(' && str[i] != ')' && str[i] != '/')) {
 	    numerator = 0;
 	    denominator = 1;
 	    NaN = true;
 	    return;
 	}
+	if(str[i] == '(')
+	    paren = true;
+    }
+    if(paren) {
+	for(int j = 0; j < str.length(); j++) {
+	    if(str[j] == '(' || str[j] == '/' || str[j] == ')')
+		str[j] = ' ';
+	}
+	iss.str(str);
+	iss >> n;
+	iss >> d;
+	numerator = n;
+	denominator = d;
+	NaN = false;
+	return;
     }
     if(sep)
-	r = * ( new Rat(stod(str)));
+	r = *(new Rat(stod(str)));
     else
 	r = *(new Rat(stoi(str)));
     if(r.denominator != 0) {
@@ -102,16 +130,31 @@ istream &operator>>(istream &is, Rat &r) {
 
 int main() {
 
-    //Rat r1 = *( new Rat());
+    Rat r1 = *( new Rat());
+    cout << "0" << endl;
     Rat r2(2);
+    cout << "2" << endl;
     Rat r3("0.5");
+    cout << "0.5" << endl;
     Rat r4("5");
+    cout << "5" << endl;
     Rat r5(2,4);
+    cout << "(1/2)" << endl;
     Rat r6(3,0);
+    cout << "NaN" << endl;
     Rat r7(0.75);
+    cout << "(3/4)" << endl;
     Rat r8("hello");
+    cout << "NaN" << endl;
 
-    //cout << "This should be (0/1): " << r1 << endl;
+    Rat r9;
+    cout << "enter rat number: ";
+    cin >> r9;
+    cout << "here is your rat number you filthy animal: " << r9 << endl;
+    cout << "end here" << endl;
+    
+    // IT FUCKING WORKS HOLY SHIT
+    cout << "This should be (0/1): " << r1 << endl;
     cout << "This should be 2: " << r2 << endl;
     cout << "This should be (1/2): " << r3 << endl;
     cout << "This should be 5: " << r4 << endl;
